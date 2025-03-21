c2afb7987cf3fdf1e0e204bcdb77f12001f53694
Olesya <fenixa@inbox.ru>
2025-03-21 12:34:47 +0300

    add admin articles urls

--- src/apps/admin/urls/urls.py
+++ src/apps/admin/urls/urls.py
@@ -17,4 +17,8 @@ urlpatterns = [
     path("edit/tag/<slug:slug>/", views.TagEditView.as_view(), name="edit-tag"),
     path("create/tag/", views.TagCreateView.as_view(), name="create-tag"),
     path('tag/delete/<slug:slug>/', views.TagDeleteView.as_view(), name="delete-tag"),
+
+    path("list-articles/", views.ListArticlesView.as_view(), name="list_articles"),
+    # path("create/article/", views.CreateArticleView.as_view(), name="create-article"),
+    # path("edit/article/<slug:slug>/", views.EditArticleView.as_view(), name="edit-article"),
 ]
